411bb3bc90b11014142e09505a4726c23cf4f141
Olivia Kesler <oakesler@protonmail.ch>
2019-08-29 00:31:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,7 +32,9 @@ class Transfer
   
   def reverse_transfer
     if self.status == "complete"
-      
-      
-      
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    end
+  end
 end
